7900565dfb52e9cc035cd19bf4801927eafc7271
EuKovs <smartkovsik@gmail.com>
2020-08-04 23:15:06 +0300

    Initial Commit

---
+++ Japanese from Scratch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Japanese from Scratch
+//
+//  Created by Kovs on 04.08.2020.
+//  Copyright © 2020 EuKovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
